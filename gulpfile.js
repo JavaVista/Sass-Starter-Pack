fe6c906dc6729ae1f817eebd15476f013d411274
Javi Carrion <suavejavi@google.com>
2020-02-21 22:54:51 -0500

    Refactor gulpfile to comfort to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,59 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create(),
+	autoprefixer = require('gulp-autoprefixer');
 
-// compile sass
-gulp.task('sass', () => {
-    return gulp.src(['src/scss/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream());
-});
+const stylePath = {
+	styles: {
+		src: 'src/scss/*.scss',
+		dest: 'src/css',
+	},
+};
+
+// compile sass and add autoprefixer
+const style = () => {
+	return gulp
+		.src(stylePath.styles.src)
+		.pipe(sass())
+		.on('error', sass.logError)
+		.pipe(
+			autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false,
+			})
+		)
+		.pipe(gulp.dest(stylePath.styles.dest))
+		.pipe(browserSync.stream());
+};
+
+// reload page
+const reload = () => browserSync.reload();
 
 // monitor & serve
-gulp.task('serve', ['sass'], () => {
-    browserSync.init({
-        server: './src'
-    });
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch(['src/*.html']).on('change', browserSync.reload);
-});
+
+const monitor = () => {
+	browserSync.init({
+		//use this directory and serve it as a mini-server
+		server: {
+			baseDir: './src',
+			// if you serving locally using something like apache
+			// use the proxy setting instead proxy: 'yourlocal.dev'
+		},
+	});
+	gulp.watch(stylePath.styles.src, style);
+	gulp.watch('src/*.html').on('change', reload);
+};
+
+// expose tasks this allows to run in the command line using i.e. gulp style
+// don't have to expose the reload function. It's currently only useful in other functions
+exports.monitor = monitor;
+exports.style = style;
+
+// Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+const build = gulp.parallel(style, monitor);
+gulp.task('build', build);
 
 // TODO: scalable you can add more task here
 
 // default
-gulp.task('default', ['serve']);
+gulp.task('default', build);
