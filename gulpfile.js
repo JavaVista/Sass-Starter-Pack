34177b6b510a7b8ed759096b8314b4bfc51a382b
Javi Carrion <suavejavi@google.com>
2020-03-03 16:47:50 -0500

    Add gulp-inject to inject all JS & CSS links

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,8 @@ const gulp = require('gulp'),
 	buffer = require('vinyl-buffer'),
 	uglify = require('gulp-uglify'),
 	imagemin = require('gulp-imagemin'),
-	newer = require('gulp-newer');
-
+	newer = require('gulp-newer'),
+	inject = require('gulp-inject');
 
 /*
 src - source files, pre-processed, un-minified.
@@ -32,14 +32,16 @@ const paths = {
 		srcSCSS: 'src/scss/**/*.scss',
 		srcJS: 'src/js/**/*.js',
 		srcJSFile: 'src/js/main',
-		srcImg: 'src/assets/**/*'
+		srcImg: 'src/assets/**/*',
 	},
 	out: {
 		dist: 'dist',
 		distIndex: 'dist/index.html',
 		distCSS: 'dist/css/',
+		distCSSFile: 'dist/css/**/*.css',
 		distJS: 'dist/js/',
-		distImg: 'dist/assets/'
+		distJSFile: 'dist/js/**/*.js',
+		distImg: 'dist/assets/',
 	},
 };
 
@@ -75,7 +77,7 @@ const styles = () => {
 const js = () => {
 	return browserify({
 		entries: [jsFiles],
-		transform: [babelify.configure({presets: ['@babel/preset-env']})] // Pass babelify as a transform and set its preset to @babel/preset-env
+		transform: [babelify.configure({ presets: ['@babel/preset-env'] })], // Pass babelify as a transform and set its preset to @babel/preset-env
 	})
 		.bundle()
 		.pipe(source('script.min.js')) // turn bundle into something which gulp understands to be able to write it to a file
@@ -88,19 +90,32 @@ const img = () => {
 	return gulp
 		.src(paths.in.srcImg)
 		.pipe(newer(paths.out.distImg)) // pass through newer images only
-		.pipe(imagemin([
-			imagemin.gifsicle({ interlaced: true }), // Interlace gif for progressive rendering
-			imagemin.mozjpeg({ progressive: true }),
-			imagemin.optipng({optimizationLevel: 5}) // levels 2 and higher enable multiple IDAT compression trials; the higher the level, the more trials
-		]))
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({ interlaced: true }), // Interlace gif for progressive rendering
+				imagemin.mozjpeg({ progressive: true }),
+				imagemin.optipng({ optimizationLevel: 5 }), // levels 2 and higher enable multiple IDAT compression trials; the higher the level, the more trials
+			])
+		)
 		.pipe(gulp.dest(paths.out.distImg));
 };
 
 // reload page
 const reload = () => browserSync.reload();
 
-// watch files & serve
+// inject Javascript files and Stylesheet files into the index.html file
+const injection = () => {
+	let sources = gulp.src([paths.out.distJSFile, paths.out.distCSSFile], {
+		read: false,
+	}); // read only the path not the file
+	let target = gulp.src(paths.out.distIndex);
 
+	return target
+		.pipe(inject(sources, { relative: true }))
+		.pipe(gulp.dest(paths.out.dist, reload()));
+};
+
+// watch files & serve
 const monitor = () => {
 	browserSync.init({
 		//use this directory and serve it as a mini-server
@@ -116,7 +131,10 @@ const monitor = () => {
 };
 
 // Specify if tasks run in series or parallel using "gulp.series" and "gulp.parallel"
-const build = gulp.parallel(gulp.series(remove, html, styles, js, img), monitor);
+const build = gulp.parallel(
+	gulp.series(remove, html, styles, img, js, injection),
+	monitor
+);
 
 // expose tasks it allows you to run in the command line "i.e. gulp style"
 // don't have to expose the reload function. It's only useful in other functions
@@ -125,5 +143,6 @@ exports.styles = styles;
 exports.js = js;
 exports.img = img;
 exports.remove = remove;
+exports.injection = injection;
 // default task just "gulp" in the command line and it runs all tasks in the build variable
 exports.default = build;
